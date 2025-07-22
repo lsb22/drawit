58bbc3eb26e79f75a3a2427487b251068b44ef2a
lsb <likith15600@gmail.com>
2025-07-22 17:32:08 +0530

    add tailwindcss

--- DrawIt/src/components/Home.jsx
+++ DrawIt/src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="">Welcome</h1>
+      <h1 className="text-amber-300">Welcome</h1>
       <div className="">Create new White Board</div>
       <div className="">Join a existing White Board</div>
     </div>
